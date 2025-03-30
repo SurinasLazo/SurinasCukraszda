04d0e1def001235b64467a63b9ec18bde8813992
SurinasLazo <lazomarcell@gmail.com>
2025-03-30 08:41:38 +0200

    Feat:Render by condition of JWT token

--- Frontend/SurinasCukraszda/src/App.jsx
+++ Frontend/SurinasCukraszda/src/App.jsx
@@ -1,26 +1,28 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Header from "./Header";
 import HomePage from "./HomePage/HomePage";
 import AboutUs from "./AboutUs/AboutUs";
 import Products from "./ProductPage/Products";
 import ProductDetail from "./ProductPage/ProductDetails/ProductDetail";
 import Cart from "./Cart/Cart";
+
 function App() {
   return (
     <Router>
+      {/* Header lokálisan kezeli a modálisokat, így itt nem kell modális állapotot fenntartani */}
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route exact path="/products" element={<Products></Products>} />
-
+        <Route path="/rolunk" element={<AboutUs />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
-        <Route
-          path="/product/:productId"
-          element={<ProductDetail></ProductDetail>}
-        />
+        <Route path="/product/:productId" element={<ProductDetail />} />
       </Routes>
     </Router>
   );
--- Frontend/SurinasCukraszda/src/Auth/RegisterModal.jsx
+++ Frontend/SurinasCukraszda/src/Auth/RegisterModal.jsx
@@ -16,7 +16,7 @@ const RegisterModal = ({ show, handleClose, switchToLogin }) => {
       return;
     }
     try {
-      await axios.post("http://localhost:5000/api/auth/register", {
+      await axios.post("http://localhost:5001/api/auth/register", {
         name,
         email,
         password,
